b5a92c88cc1ef2ac9dd8d1b2c079ac437837dd3c
pmihaly <misi@pappmihaly.com>
2021-09-02 05:23:46 +0200

    refactor: :fire: user erőforrás és auth törlése

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,12 @@ process.env['NODE_CONFIG_DIR'] = __dirname + '/configs';
 
 import 'dotenv/config';
 import App from '@/app';
-import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
-import UsersRoute from '@routes/users.route';
 import PicturesRoute from '@routes/pictures.route';
 import validateEnv from '@utils/validateEnv';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new PicturesRoute()]);
+const app = new App([new IndexRoute(), new PicturesRoute()]);
 
 app.listen();
