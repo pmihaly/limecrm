1e86f038c6ace0ed888b41af74cf2570b8526ecd
pmihaly <misi@pappmihaly.com>
2021-09-02 11:19:30 +0200

    docs: :memo: interaktív api docs

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,6 @@ import helmet from 'helmet';
 import hpp from 'hpp';
 import morgan from 'morgan';
 import { connect, set } from 'mongoose';
-import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import { dbConnection } from '@databases';
 import { Routes } from '@interfaces/routes.interface';
@@ -73,19 +72,7 @@ class App {
   }
 
   private initializeSwagger() {
-    const options = {
-      swaggerDefinition: {
-        info: {
-          title: 'REST API',
-          version: '1.0.0',
-          description: 'Example docs',
-        },
-      },
-      apis: ['swagger.yaml'],
-    };
-
-    const specs = swaggerJSDoc(options);
-    this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+    this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(require('../swagger.json')));
   }
 
   private initializeErrorHandling() {
