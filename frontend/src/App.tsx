33138eeb53f97fc40bf7e651e715c35012be48a7
pmihaly <misi@pappmihaly.com>
2021-09-04 21:54:42 +0200

    refactor: :recycle: kártya grid és kárty különválasztása

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,15 @@
 import AppBar from '@material-ui/core/AppBar';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import IconButton from '@material-ui/core/IconButton';
-import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import LinkIcon from '@material-ui/icons/Link';
 import CameraIcon from '@material-ui/icons/PhotoCamera';
 import React, { useEffect, useState } from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
 import Button from '@material-ui/core/Button';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import PictureCard from './components/PictureCard';
 
 const useStyles = makeStyles(theme => ({
   icon: {
@@ -45,41 +26,20 @@ const useStyles = makeStyles(theme => ({
     paddingTop: theme.spacing(8),
     paddingBottom: theme.spacing(8),
   },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  title: {
-    fontSize: 14,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
 }));
-
-function fileSize(size: number) {
-  const i: number = Math.floor(Math.log(size) / Math.log(1024));
-  const fixed: any = (size / Math.pow(1024, i)).toFixed(2);
-  return fixed * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
-}
 
 export default function Album() {
   const classes = useStyles();
 
   const [pictures, setPictures] = useState<any[]>([]);
 
-  const [selectedPictureId, setSelectedPictureId] = useState<any>(0);
-
   const [snackbarOpen, setSnackbarOpen] = useState<boolean>(false);
   const [snackbarMessage, setSnackbarMessage] = useState<String>('');
+
+  function showCopiedSnackbar() {
+    setSnackbarMessage('Kép URL vágólapra másolva');
+    setSnackbarOpen(true);
+  }
 
   useEffect(() => {
     fetch('/pictures')
@@ -115,37 +75,7 @@ export default function Album() {
           />
           <Grid container spacing={4} alignItems="flex-start">
             {pictures.map(picture => (
-              <Grid item key={picture._id} xs={12} sm={6} md={4}>
-                <Card className={classes.card} onMouseEnter={() => setSelectedPictureId(picture._id)} onMouseLeave={() => setSelectedPictureId(0)}>
-                  <CardHeader
-                    action={
-                      <>
-                        <IconButton
-                          aria-label="copy picture url"
-                          onClick={() => {
-                            navigator.clipboard.writeText(`${process.env.REACT_APP_API_URL}/${picture.filename}`);
-                            setSnackbarMessage('Kép URL vágólapra másolva');
-                            setSnackbarOpen(true);
-                          }}
-                        >
-                          <LinkIcon />
-                        </IconButton>
-                      </>
-                    }
-                  />
-                  <a href={`${process.env.REACT_APP_API_URL}/${picture.filename}`} target="_blank" rel="noopener noreferrer">
-                    <CardMedia className={classes.cardMedia} image={`/${picture.filename}`} title={picture.filename} />
-                  </a>
-                  {picture._id === selectedPictureId && (
-                    <CardContent className={classes.cardContent}>
-                      <Typography className={classes.title} color="textSecondary" gutterBottom>
-                        {picture.uploaderIp}, {picture.pictureDimensions.width}x{picture.pictureDimensions.height}, {fileSize(picture.filesize)}
-                      </Typography>
-                      <Typography>{picture.description}</Typography>
-                    </CardContent>
-                  )}
-                </Card>
-              </Grid>
+              <PictureCard picture={picture} showCopiedSnackbar={showCopiedSnackbar}></PictureCard>
             ))}
           </Grid>
         </Container>
---
+++ frontend/src/components/PictureCard.tsx
@@ -0,0 +1,88 @@
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import LinkIcon from '@material-ui/icons/Link';
+import { useState } from 'react';
+
+function fileSize(size: number): string {
+  const i: number = Math.floor(Math.log(size) / Math.log(1024));
+  const fixed: any = (size / Math.pow(1024, i)).toFixed(2);
+  return fixed * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+}
+
+export interface IPictureCardProps {
+  picture: IPicture;
+  showCopiedSnackbar(): void;
+}
+
+interface IPicture {
+  _id: string;
+  description: string;
+  filename: string;
+  filesize: number;
+  pictureDimensions: { width: number; height: number };
+  uploadDate: string;
+  uploaderIp: string;
+}
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  title: {
+    fontSize: 14,
+  },
+}));
+
+export default function PictureCard(props: IPictureCardProps) {
+  const classes = useStyles();
+
+  const [cardSelected, setCardSelected] = useState(false);
+
+  return (
+    <Grid item key={props.picture._id} xs={12} sm={6} md={4}>
+      <Card className={classes.card} onMouseEnter={() => setCardSelected(true)} onMouseLeave={() => setCardSelected(false)}>
+        <CardHeader
+          action={
+            <>
+              <IconButton
+                aria-label="copy picture url"
+                onClick={() => {
+                  navigator.clipboard.writeText(`${process.env.REACT_APP_API_URL}/${props.picture.filename}`);
+                  props.showCopiedSnackbar();
+                }}
+              >
+                <LinkIcon />
+              </IconButton>
+            </>
+          }
+        />
+        <a href={`${process.env.REACT_APP_API_URL}/${props.picture.filename}`} target="_blank" rel="noopener noreferrer">
+          <CardMedia className={classes.cardMedia} image={`/${props.picture.filename}`} title={props.picture.filename} />
+        </a>
+        {cardSelected && (
+          <CardContent className={classes.cardContent}>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              {props.picture.uploaderIp}, {props.picture.pictureDimensions.width}x{props.picture.pictureDimensions.height},{' '}
+              {fileSize(props.picture.filesize)}
+            </Typography>
+            <Typography>{props.picture.description}</Typography>
+          </CardContent>
+        )}
+      </Card>
+    </Grid>
+  );
+}
